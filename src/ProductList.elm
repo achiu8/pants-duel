6d063777950a006b2529b1398ba7eb763547b183
Andy Chiu <achiu8@gmail.com>
2019-03-06 23:37:55 -0500

    factor out product list

---
+++ src/ProductList.elm
@@ -0,0 +1,57 @@
+module ProductList exposing (..)
+
+import Bootstrap.Grid as Grid
+import Bootstrap.Grid.Col as Col
+import Bootstrap.Text as Text
+import FontAwesome.Attributes as Icon
+import FontAwesome.Icon as Icon
+import FontAwesome.Solid as Icon
+import Html exposing (..)
+import Html.Attributes exposing (..)
+import Html.Events exposing (..)
+
+import Models exposing (..)
+import Msgs exposing (..)
+import Utils exposing (..)
+
+view : Model -> Html Msg
+view model =
+  model.products
+    |> List.filter (\p -> p.category == model.category)
+    |> List.map (productRow model.roster)
+    |> div []
+
+productRow : List Product -> Product -> Html Msg
+productRow roster product =
+  let selected = List.member product roster
+      selectable = budgetLeft roster >= product.price
+      action = if selected
+                 then RemoveProduct
+                 else if selectable
+                   then SelectProduct
+                   else (always NoOp)
+  in
+  div [ style "margin-top" "20px" ]
+    [ Grid.row []
+      [ Grid.col [ Col.xs3 ]
+        [ img [ src (categoryImage product.category) , style "height" "100px" ] []
+        ]
+      , Grid.col [ Col.middleXs, Col.xs6 ]
+        [ div []
+          [ div [] [ text product.name ]
+          , div [] [ text (String.fromInt product.price) ]
+          ]
+        ]
+      , Grid.col [ Col.middleXs, Col.textAlign Text.alignXsRight ]
+        [ div [ onClick (action product) ]
+          [ if selected
+              then Icon.viewStyled [ Icon.fa2x, style "color" "Green" ] Icon.checkCircle
+              else Icon.viewStyled
+                     [ Icon.fa2x
+                     , style "color" (if selectable then "black" else "lightgrey")
+                     ]
+                     Icon.plusCircle
+          ]
+        ]
+      ]
+    ]
--- src/Roster.elm
+++ src/Roster.elm
@@ -5,15 +5,13 @@ import Bootstrap.Grid as Grid
 import Bootstrap.Grid.Col as Col
 import Bootstrap.Form.Select as Select
 import Bootstrap.Text as Text
-import FontAwesome.Attributes as Icon
-import FontAwesome.Icon as Icon
-import FontAwesome.Solid as Icon
 import Html exposing (..)
 import Html.Attributes exposing (..)
 import Html.Events exposing (..)
 
 import Models exposing (..)
 import Msgs exposing (..)
+import ProductList exposing (..)
 import Utils exposing (..)
 
 view : Model -> Html Msg
@@ -40,7 +38,7 @@ view model =
         ]
       ]
       (List.map (selectOption << categoryDisplay) categories)
-    , productsList model
+    , ProductList.view model
     ]
 
 header : String -> Html Msg
@@ -56,44 +54,3 @@ selectOption : String -> Select.Item Msg
 selectOption v =
   Select.item [] [ text v ]
 
-productsList : Model -> Html Msg
-productsList model =
-  model.products
-    |> List.filter (\p -> p.category == model.category)
-    |> List.map (productRow model.roster)
-    |> div []
-
-productRow : List Product -> Product -> Html Msg
-productRow roster product =
-  let selected = List.member product roster
-      selectable = budgetLeft roster >= product.price
-      action = if selected
-                 then RemoveProduct
-                 else if selectable
-                   then SelectProduct
-                   else (always NoOp)
-  in
-  div [ style "margin-top" "20px" ]
-    [ Grid.row []
-      [ Grid.col []
-        [ img [ src (categoryImage product.category) , style "height" "100px" ] []
-        ]
-      , Grid.col [ Col.middleXs ]
-        [ div []
-          [ div [] [ text product.name ]
-          , div [] [ text (String.fromInt product.price) ]
-          ]
-        ]
-      , Grid.col [ Col.middleXs, Col.textAlign Text.alignXsRight ]
-        [ div [ onClick (action product) ]
-          [ if selected
-              then Icon.viewStyled [ Icon.fa2x, style "color" "Green" ] Icon.checkCircle
-              else Icon.viewStyled
-                     [ Icon.fa2x
-                     , style "color" (if selectable then "black" else "lightgrey")
-                     ]
-                     Icon.plusCircle
-          ]
-        ]
-      ]
-    ]
